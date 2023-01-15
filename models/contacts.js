bffa82e206a83718d43cf75aa276f5d356a423c6
Alexandr1899 <alex.sinitsin1899@gmail.com>
2023-01-15 18:37:24 +0200

    add hw

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,55 @@
-// const fs = require('fs/promises')
+const { Contacts } = require("../db/contactModel");
 
-const listContacts = async () => {}
+const getContacts = async () => {
+  const contacts = await Contacts.find({});
+  return { contacts };
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contact = await Contacts.findById(contactId);
 
-const removeContact = async (contactId) => {}
+  if (!contactId) {
+    return { message: "Not found" };
+  }
 
-const addContact = async (body) => {}
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  await Contacts.findByIdAndRemove(contactId);
+
+  if (!contactId) {
+    return { message: "Not found" };
+  }
+
+  return { message: `The contact was removed` };
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const contact = new Contacts({ name, email, phone });
+  await contact.save();
+  return contact;
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  await Contacts.findByIdAndUpdate(contactId, { $set: { name, email, phone } });
+
+  return { message: "The contact was updated" };
+};
+
+const changeContactStatus = async (contactId, status) => {
+  await Contacts.findByIdAndUpdate(contactId, { $set: { favorite: status } });
+
+  return { message: "The status was changed" };
+};
 
 module.exports = {
-  listContacts,
+  getContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+  changeContactStatus,
+};
